4a2dd74bd8c76928b755499909cf42af4ef2ce73
RettichLP <rettichlp01@gmail.com>
2025-08-30 23:29:08 +0200

    feat: integrate IHudRenderListener into BombListener for bomb timer display

--- src/client/java/de/rettichlp/pkutils/PKUtilsClient.java
+++ src/client/java/de/rettichlp/pkutils/PKUtilsClient.java
@@ -5,18 +5,11 @@ import de.rettichlp.pkutils.common.services.ActivityService;
 import de.rettichlp.pkutils.common.services.FactionService;
 import de.rettichlp.pkutils.common.services.SyncService;
 import de.rettichlp.pkutils.common.storage.Storage;
-import de.rettichlp.pkutils.listener.impl.faction.BombListener;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-
-import java.util.concurrent.TimeUnit;
 
 public class PKUtilsClient implements ClientModInitializer {
 
@@ -49,27 +42,6 @@ public class PKUtilsClient implements ClientModInitializer {
 
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             this.registry.registerCommands(dispatcher);
-        });
-
-        HudRenderCallback.EVENT.register((drawContext, tickCounter) -> {
-            if (BombListener.bombTimerActive) {
-                MinecraftClient client = MinecraftClient.getInstance();
-                long elapsedTime = System.currentTimeMillis() - BombListener.bombPlantedTimestamp;
-                long minutes = TimeUnit.MILLISECONDS.toMinutes(elapsedTime);
-                long seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime) % 60;
-
-                Text timerText = Text.empty()
-                        .append(Text.literal("Bombe: ").formatted(Formatting.RED, Formatting.BOLD))
-                        .append(Text.literal(BombListener.bombLocation).formatted(Formatting.GOLD, Formatting.BOLD))
-                        .append(Text.literal(" | ").formatted(Formatting.GRAY))
-                        .append(Text.literal(String.format("%02d:%02d", minutes, seconds)).formatted(Formatting.RED, Formatting.BOLD));
-
-                int textWidth = client.textRenderer.getWidth(timerText);
-                int x = (client.getWindow().getScaledWidth() - textWidth) / 2;
-                int y = 15; // Position auf der Y-Achse
-
-                drawContext.drawTextWithShadow(client.textRenderer, timerText, x, y, 0xFFFFFF);
-            }
         });
     }
 }
