4796e0b868b7bd441ad1e6f1e1191a5e3c5a0739
RettichLP <rettichlp01@gmail.com>
2025-08-31 15:15:43 +0200

    feat: enhance FactionListener to track reinforcements and handle movement events

--- src/client/java/de/rettichlp/pkutils/listener/impl/faction/FactionListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/faction/FactionListener.java
@@ -4,21 +4,34 @@ import de.rettichlp.pkutils.common.api.schema.ActivityType;
 import de.rettichlp.pkutils.common.registry.PKUtilsBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsListener;
 import de.rettichlp.pkutils.common.storage.Storage;
+import de.rettichlp.pkutils.common.storage.schema.Reinforcement;
 import de.rettichlp.pkutils.listener.IMessageReceiveListener;
 import de.rettichlp.pkutils.listener.IMessageSendListener;
+import de.rettichlp.pkutils.listener.IMoveListener;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.mojang.text2speech.Narrator.LOGGER;
 import static de.rettichlp.pkutils.PKUtilsClient.activityService;
 import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
 import static de.rettichlp.pkutils.PKUtilsClient.player;
 import static de.rettichlp.pkutils.PKUtilsClient.storage;
 import static de.rettichlp.pkutils.common.storage.Storage.ToggledChat.NONE;
+import static java.lang.Integer.parseInt;
+import static java.util.Comparator.comparing;
+import static java.util.Objects.nonNull;
 import static java.util.Optional.ofNullable;
 import static java.util.regex.Pattern.compile;
+import static net.minecraft.text.ClickEvent.Action.RUN_COMMAND;
 import static net.minecraft.text.Text.empty;
 import static net.minecraft.text.Text.of;
 import static net.minecraft.util.Formatting.AQUA;
@@ -28,7 +41,7 @@ import static net.minecraft.util.Formatting.GRAY;
 import static net.minecraft.util.Formatting.RED;
 
 @PKUtilsListener
-public class FactionListener extends PKUtilsBase implements IMessageReceiveListener, IMessageSendListener {
+public class FactionListener extends PKUtilsBase implements IMessageReceiveListener, IMessageSendListener, IMoveListener {
 
     private static final Pattern REINFORCEMENT_PATTERN = compile("^(?:(?<type>.+)! )?(?<sender>.+) benötigt Unterstützung in der Nähe von (?<naviPoint>.+) \\((?<distance>\\d+)m\\)!$");
     private static final Pattern REINFORCEMENT_BUTTON_PATTERN = compile("^ §7» §cRoute anzeigen §7\\| §cUnterwegs$");
@@ -60,9 +73,12 @@ public class FactionListener extends PKUtilsBase implements IMessageReceiveListe
             String naviPoint = reinforcementMatcher.group("naviPoint");
             String distance = reinforcementMatcher.group("distance");
 
-            Text reinforcement = REINFORCEMENT.create(type, sender, naviPoint, distance);
+            Text reinforcementText = REINFORCEMENT.create(type, sender, naviPoint, distance);
             player.sendMessage(empty(), false);
-            player.sendMessage(reinforcement, false);
+            player.sendMessage(reinforcementText, false);
+
+            Reinforcement reinforcement = new Reinforcement(type, sender, naviPoint, distance);
+            storage.getReinforcements().add(reinforcement);
 
             return false;
         }
@@ -71,6 +87,44 @@ public class FactionListener extends PKUtilsBase implements IMessageReceiveListe
         if (reinforcementButtonMatcher.find()) {
             // send empty line after buttons
             MinecraftClient.getInstance().execute(() -> player.sendMessage(empty(), false));
+
+            List<ClickEvent> clickEvents = text.getSiblings().stream()
+                    .map(Text::getStyle)
+                    .map(Style::getClickEvent)
+                    .filter(Objects::nonNull)
+                    .filter(clickEvent -> clickEvent.getAction() == RUN_COMMAND)
+                    .toList();
+
+            // origin reinforcement sender retrieving
+            String senderName = clickEvents.stream()
+                    .map(ClickEvent::getValue)
+                    .filter(commandString -> commandString.startsWith("/reinf onway "))
+                    .map(commandString -> commandString.replace("/reinf onway ", ""))
+                    .toList().getFirst();
+
+            // block position retrieving
+            BlockPos blockPos = clickEvents.stream()
+                    .map(ClickEvent::getValue)
+                    .filter(commandString -> commandString.startsWith("/navi "))
+                    .map(commandString -> commandString.replace("/navi ", ""))
+                    .map(naviArguments -> {
+                        String[] split = naviArguments.split(" ");
+                        int x = parseInt(split[0]);
+                        int y = parseInt(split[1]);
+                        int z = parseInt(split[2]);
+                        return new BlockPos(x, y, z);
+                    })
+                    .toList().getFirst();
+
+            Optional<Reinforcement> optionalReinforcement = storage.getReinforcements().stream()
+                    .filter(reinforcement -> reinforcement.getPlayerName().equals(senderName))
+                    .max(comparing(Reinforcement::getCreatedAt));
+
+            optionalReinforcement.ifPresent(reinforcement -> reinforcement.setBlockPos(blockPos));
+
+            LOGGER.info("Reinforcement button detected, stored block position {} for sender {}", blockPos, senderName);
+            LOGGER.info("Last reinforcement of sender: {}", optionalReinforcement.orElse(null));
+
             return true;
         }
 
@@ -83,10 +137,16 @@ public class FactionListener extends PKUtilsBase implements IMessageReceiveListe
             Text reinforcementAnswer = REINFORCEMENT_ON_THE_WAY.create(sender, target, distance);
             player.sendMessage(reinforcementAnswer, false);
 
-            String[] splitSender = sender.split(" ");
-            String lastWord = splitSender[splitSender.length - 1];
-            if (player.getName().getString().equals(lastWord)) {
-                activityService.trackActivity(ActivityType.REINFORCEMENT);
+            Optional<Reinforcement> optionalReinforcement = storage.getReinforcements().stream()
+                    .filter(reinforcement -> reinforcement.getPlayerName().equals(target)) // get reinforcements of target
+                    .max(comparing(Reinforcement::getCreatedAt)); // get the latest one
+
+            if (optionalReinforcement.isPresent()) {
+                Reinforcement reinforcement = optionalReinforcement.get();
+
+                String[] splitSender = sender.split(" ");
+                String lastWord = splitSender[splitSender.length - 1]; // "Polizei RettichLP" -> "RettichLP"
+                reinforcement.getAcceptedPlayerNames().add(lastWord);
             }
 
             return false;
@@ -104,6 +164,27 @@ public class FactionListener extends PKUtilsBase implements IMessageReceiveListe
         }
 
         return true;
+    }
+
+    @Override
+    public void onMove(BlockPos blockPos) {
+        // get the nearest reinforcement within 60 blocks that is not from yourself
+        Optional<Reinforcement> optionalReinforcement = storage.getReinforcements().stream()
+                .filter(reinforcement -> nonNull(reinforcement.getBlockPos())) // check if block position is set
+                .filter(reinforcement -> reinforcement.getBlockPos().isWithinDistance(player.getBlockPos(), 60))
+                .filter(reinforcement -> !reinforcement.getPlayerName().equals(player.getGameProfile().getName()))
+                .max(comparing(Reinforcement::getCreatedAt));
+
+        optionalReinforcement.ifPresent(reinforcement -> {
+            if (reinforcement.isAddedAsActivity()) {
+                LOGGER.info("Reinforcement already added as activity");
+                return;
+            }
+
+            reinforcement.setAddedAsActivity(true);
+            activityService.trackActivity(ActivityType.REINFORCEMENT);
+            LOGGER.info("Reinforcement reached, tracked activity");
+        });
     }
 
     @FunctionalInterface
