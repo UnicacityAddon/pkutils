8e2d621008a01c3ecd0437cc2975ebaf0afdb451
RettichLP <rettichlp01@gmail.com>
2025-08-31 20:03:43 +0200

    feat: update DepositCommand to handle player money amount correctly

--- src/client/java/de/rettichlp/pkutils/command/DepositCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/DepositCommand.java
@@ -5,53 +5,49 @@ import de.rettichlp.pkutils.common.registry.CommandBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
 import de.rettichlp.pkutils.listener.IMessageReceiveListener;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
-import static de.rettichlp.pkutils.PKUtilsClient.player;
+import static java.lang.Integer.parseInt;
 import static java.util.regex.Pattern.compile;
 
 @PKUtilsCommand(label = "einzahlen")
 public class DepositCommand extends CommandBase implements IMessageReceiveListener {
 
-    private int amount = 0;
+    private static final Pattern PLAYER_MONEY_AMOUNT_PATTERN = compile("^ - Geld: (?<moneyAmount>\\d+)\\$$");
 
-    private static final Pattern PLAYER_MONEY_BANK_AMOUNT = compile("^ - Geld: (?<moneyAmount>\\d+)\\$$");
+    private int amount = 0;
 
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
+        return node
+                .executes(context -> {
+                    networkHandler.sendChatCommand("stats");
 
-        networkHandler.sendChatCommand("stats");
+                    delayedAction(() -> {
+                        if (this.amount <= 0) {
+                            sendModMessage("Du hast kein Geld zum Einzahlen.", false);
+                            return;
+                        }
 
-        delayedAction(() -> {
+                        networkHandler.sendChatCommand("bank einzahlen " + this.amount);
+                    }, 1000);
 
-            if (this.amount == 0) {
-                sendModMessage("Du hast kein Geld zum Einzahlen.", false);
-                return;
-            }
-
-            networkHandler.sendChatCommand("bank einzahlen " + this.amount);
-
-        }, 1000);
-
-        return null;
+                    return 1;
+                });
     }
 
     @Override
-    public boolean onMessageReceive(String message) {
-        Matcher matcher = PLAYER_MONEY_BANK_AMOUNT.matcher(message);
-
-        if (matcher.find()) {
-            try {
-                this.amount = Integer.parseInt(matcher.group("moneyAmount"));
-            } catch (NumberFormatException ignored) {
-                ignored.printStackTrace();
-            }
+    public boolean onMessageReceive(Text text, String message) {
+        Matcher playerMoneyAmountMatcher = PLAYER_MONEY_AMOUNT_PATTERN.matcher(message);
+        if (playerMoneyAmountMatcher.find()) {
+            this.amount = parseInt(playerMoneyAmountMatcher.group("moneyAmount"));
         }
 
-        return false;
+        return true;
     }
 }
