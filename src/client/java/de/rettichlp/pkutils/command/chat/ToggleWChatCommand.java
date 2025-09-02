0b054e8d2cd62b6d1f949e5e480d8a71ac503772
RettichLP <rettichlp01@gmail.com>
2025-09-02 19:11:44 +0200

    refactor: reorganize command packages for better structure and clarity

--- src/client/java/de/rettichlp/pkutils/command/chat/ToggleWChatCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/chat/ToggleWChatCommand.java
@@ -1,4 +1,4 @@
-package de.rettichlp.pkutils.command;
+package de.rettichlp.pkutils.command.chat;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.rettichlp.pkutils.common.registry.CommandBase;
@@ -10,17 +10,17 @@ import org.jetbrains.annotations.NotNull;
 import static de.rettichlp.pkutils.PKUtilsClient.hudService;
 import static de.rettichlp.pkutils.PKUtilsClient.storage;
 import static de.rettichlp.pkutils.common.services.HudService.NotificationType.DEFAULT;
-import static de.rettichlp.pkutils.common.storage.Storage.ToggledChat.F_CHAT;
 import static de.rettichlp.pkutils.common.storage.Storage.ToggledChat.NONE;
+import static de.rettichlp.pkutils.common.storage.Storage.ToggledChat.W_CHAT;
 
-@PKUtilsCommand(label = "ff")
-public class ToggleFChatCommand extends CommandBase {
+@PKUtilsCommand(label = "ww")
+public class ToggleWChatCommand extends CommandBase {
 
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
         return node
                 .executes(context -> {
-                    Storage.ToggledChat newState = storage.getToggledChat() == F_CHAT ? NONE : F_CHAT;
+                    Storage.ToggledChat newState = storage.getToggledChat() == W_CHAT ? NONE : W_CHAT;
                     storage.setToggledChat(newState);
                     hudService.sendNotification(newState.getToggleMessage(), DEFAULT);
                     return 1;
