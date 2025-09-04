410ebaed571b189fc851642dbfe155ccac86c112
RettichLP <rettichlp01@gmail.com>
2025-09-04 14:51:00 +0200

    refactor: update Request class to use requestData and simplify send method

--- src/client/java/de/rettichlp/pkutils/common/api/Request.java
+++ src/client/java/de/rettichlp/pkutils/common/api/Request.java
@@ -1,8 +1,6 @@
 package de.rettichlp.pkutils.common.api;
 
 import com.google.gson.Gson;
-import de.rettichlp.pkutils.common.api.schema.ErrorResponse;
-import de.rettichlp.pkutils.common.api.schema.Response;
 import lombok.Builder;
 import net.minecraft.client.MinecraftClient;
 
@@ -10,8 +8,8 @@ import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
-import java.util.function.Consumer;
 
 import static de.rettichlp.pkutils.PKUtils.LOGGER;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -23,34 +21,24 @@ public class Request<T extends IRequest> {
     private static final Gson GSON = new Gson();
     private static final String SESSION_TOKEN = MinecraftClient.getInstance().getSession().getAccessToken();
 
-    private final T body;
+    private final T requestData;
 
-    public void send(Consumer<Response> successCallback, Consumer<ErrorResponse> failureCallback) {
-        supplyAsync(() -> {
+    public CompletableFuture<HttpResponse<String>> send() {
+        return supplyAsync(() -> {
             try {
                 HttpRequest httpRequest = getHttpRequest();
                 HttpResponse<String> httpResponse = HTTP_CLIENT.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-
-                if (httpResponse.statusCode() >= 200 && httpResponse.statusCode() < 300) {
-                    LOGGER.info("Request successful: {} -> [{}] {}", httpRequest, httpResponse.statusCode(), httpResponse.body());
-                    Response response = GSON.fromJson(httpResponse.body(), Response.class);
-                    successCallback.accept(response);
-                } else {
-                    LOGGER.warn("Request failed: {} -> [{}] {}", httpRequest, httpResponse.statusCode(), httpResponse.body());
-                    ErrorResponse response = GSON.fromJson(httpResponse.body(), ErrorResponse.class);
-                    failureCallback.accept(response);
-                }
+                LOGGER.info("Sent request: {} -> [{}] {}", httpRequest, httpResponse.statusCode(), httpResponse.body());
+                return httpResponse;
             } catch (IOException | InterruptedException e) {
                 throw new CompletionException(e);
             }
-
-            return null; // return nothing, everything was already handled in the callbacks
         });
     }
 
     private HttpRequest getHttpRequest() {
         return HttpRequest.newBuilder()
-                .uri(this.body.getUrl())
+                .uri(this.requestData.getUrl())
                 .header("Content-Type", "application/json")
                 .header("X-Minecraft-Session-Token", SESSION_TOKEN)
                 .POST(HttpRequest.BodyPublishers.ofString(getJsonBody()))
@@ -58,6 +46,6 @@ public class Request<T extends IRequest> {
     }
 
     private String getJsonBody() {
-        return GSON.toJson(this.body);
+        return GSON.toJson(this.requestData);
     }
 }
