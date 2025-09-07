cd218e8b866466eed20887acd2b7181f3bbe07cf
RettichLP <rettichlp01@gmail.com>
2025-09-07 23:34:54 +0200

    refactor: simplify faction argument handling in MiCommand

--- src/client/java/de/rettichlp/pkutils/command/MiCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/MiCommand.java
@@ -4,19 +4,17 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.rettichlp.pkutils.common.registry.CommandBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
 import de.rettichlp.pkutils.common.storage.schema.Faction;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import static com.mojang.brigadier.arguments.StringArgumentType.*;
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
 import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
 import static de.rettichlp.pkutils.PKUtilsClient.player;
 import static de.rettichlp.pkutils.PKUtilsClient.storage;
+import static de.rettichlp.pkutils.common.storage.schema.Faction.fromDisplayName;
+import static java.util.Arrays.stream;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.minecraft.command.CommandSource.suggestMatching;
 
 @PKUtilsCommand(label = "mi")
@@ -25,28 +23,17 @@ public class MiCommand extends CommandBase {
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
         return node
-                .then(ClientCommandManager.argument("faction", greedyString())
-                        .suggests((context, builder) -> {
-                            String remaining = builder.getRemaining().toLowerCase();
-
-                            Arrays.stream(Faction.values())
-                                    .map(Faction::getDisplayName)
-                                    .filter(name -> name.toLowerCase().contains(remaining))
-                                    .forEach(builder::suggest);
-
-                            return builder.buildFuture();
-                        })
+                .then(argument("faction", greedyString())
+                        .suggests((context, builder) -> suggestMatching(stream(Faction.values())
+                                .map(Faction::getDisplayName), builder))
                         .executes(context -> {
                             String input = getString(context, "faction");
-                            Optional<Faction> factionOpt = Faction.fromDisplayName(input);
-
-                            if (factionOpt.isEmpty()) {
+                            fromDisplayName(input).ifPresentOrElse(faction -> {
+                                networkHandler.sendChatCommand("memberinfo " + faction.getMemberInfoCommandName());
+                            }, () -> {
                                 sendModMessage("Die Fraktion" + input + " konnte nicht gefunden werden.", false);
-                                return 0;
-                            }
+                            });
 
-                            Faction faction = factionOpt.get();
-                            networkHandler.sendChatCommand("memberinfo " + faction.getMemberInfoCommandName());
                             return 1;
                         })
                 )
