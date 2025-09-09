c16ba53ad14434a6e1a6929a235444c442080a94
RettichLP <rettichlp01@gmail.com>
2025-09-09 12:52:47 +0200

    feat: add TodoCommand for managing tasks with add, delete, and done functionalities

--- src/client/java/de/rettichlp/pkutils/common/models/config/MainConfig.java
+++ src/client/java/de/rettichlp/pkutils/common/models/config/MainConfig.java
@@ -2,7 +2,11 @@ package de.rettichlp.pkutils.common.models.config;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class MainConfig {
 
+    private List<TodoEntry> todos = new ArrayList<>();
 }
