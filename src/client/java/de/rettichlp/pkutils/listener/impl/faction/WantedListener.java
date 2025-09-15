b867e33c688f80bfd8c3c66f5be2b445ea218447
RettichLP <rettichlp01@gmail.com>
2025-09-15 15:08:00 +0200

    fix: update PARKTICKET_PATTERN to capture vehicle plate and enhance message formatting

--- src/client/java/de/rettichlp/pkutils/listener/impl/faction/WantedListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/faction/WantedListener.java
@@ -52,7 +52,7 @@ public class WantedListener extends PKUtilsBase implements IMessageReceiveListen
     private static final Pattern LICENSE_GUN_TAKE_PATTERN = compile("^(Agent|Agentin|Beamter|Beamtin) (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+)(?:'s)* Waffenschein abgenommen\\.$");
     private static final Pattern TAKE_GUNS_PATTERN = compile("^(Beamter|Beamtin) (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) (seine|ihre) Waffen abgenommen\\.$");
     private static final Pattern TAKE_DRUGS_PATTERN = compile("^(Beamter|Beamtin) (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) (seine|ihre) Drogen abgenommen.$");
-    private static final Pattern PARKTICKET_PATTERN = compile("^HQ: (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat ein Strafzettel an das Fahrzeug \\[[A-Z0-9-]+] vergeben\\.$");
+    private static final Pattern PARKTICKET_PATTERN = compile("^HQ: (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat ein Strafzettel an das Fahrzeug \\[(?<plate>[A-Z0-9-]+)] vergeben\\.$");
     private static final Pattern SEARCH_TRUNK_PATTERN = compile("^HQ: (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat den Kofferraum vom Fahrzeug (?<plate>.+) durchsucht, over\\.$");
     private static final Pattern TRACKER_AGENT_PATTERN = compile("^HQ: (Agent|Agentin) (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat ein Peilsender an (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) befestigt, over\\.$");
 
@@ -200,12 +200,22 @@ public class WantedListener extends PKUtilsBase implements IMessageReceiveListen
         Matcher parkticketMatcher = PARKTICKET_PATTERN.matcher(message);
         if (parkticketMatcher.find()) {
             String officerName = parkticketMatcher.group("playerName");
+            String plate = parkticketMatcher.group("plate");
+
+            Text modifiedMessage = empty()
+                    .append(of("Strafzettel").copy().formatted(RED)).append(" ")
+                    .append(of("-").copy().formatted(GRAY)).append(" ")
+                    .append(of(plate).copy().formatted(BLUE)).append(" ")
+                    .append(of("-").copy().formatted(GRAY)).append(" ")
+                    .append(of(officerName).copy().formatted(BLUE));
+
+            player.sendMessage(modifiedMessage, false);
 
             if (clientPlayerName.equals(officerName)) {
                 api.trackActivity(PARK_TICKET);
             }
 
-            return true;
+            return false;
         }
 
         Matcher searchTrunkMatcher = SEARCH_TRUNK_PATTERN.matcher(message);
