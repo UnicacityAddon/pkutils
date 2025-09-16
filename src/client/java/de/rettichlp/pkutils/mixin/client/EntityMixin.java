8119142b32d02e572af363b0d6b21a811b4cdcd5
RettichLP <rettichlp01@gmail.com>
2025-09-16 21:55:43 +0200

    feat: enhance player entry vehicle callback with UUID check

--- src/client/java/de/rettichlp/pkutils/mixin/client/EntityMixin.java
+++ src/client/java/de/rettichlp/pkutils/mixin/client/EntityMixin.java
@@ -2,22 +2,27 @@ package de.rettichlp.pkutils.mixin.client;
 
 import de.rettichlp.pkutils.listener.callback.PlayerEnterVehicleCallback;
 import net.minecraft.entity.Entity;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.entity.EntityLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static de.rettichlp.pkutils.PKUtilsClient.player;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
 
     @Inject(method = "startRiding(Lnet/minecraft/entity/Entity;Z)Z", at = @At("RETURN"))
     private void onStartRiding(Entity vehicle, boolean force, CallbackInfoReturnable<Boolean> cir) {
-        if (cir.getReturnValue()) { // only for successful start riding
-            Entity self = (Entity) (Object) this;
-            if (self instanceof ServerPlayerEntity) {
-                PlayerEnterVehicleCallback.EVENT.invoker().onEnter(vehicle);
-            }
+        // only for successful start riding
+        if (!cir.getReturnValue()) {
+            return;
+        }
+
+        EntityLike self = (Entity) (Object) this;
+        if (self.getUuid().equals(player.getUuid())) {
+            PlayerEnterVehicleCallback.EVENT.invoker().onEnter(vehicle);
         }
     }
 }
