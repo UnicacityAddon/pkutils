a419ec66bb66a4494131e5ce5b49f8e1d61e72c1
RettichLP <rettichlp01@gmail.com>
2025-09-18 23:37:25 +0200

    feat: implement countdown rendering in HUD for active countdowns

--- src/client/java/de/rettichlp/pkutils/listener/impl/HudListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/HudListener.java
@@ -1,24 +1,76 @@
 package de.rettichlp.pkutils.listener.impl;
 
+import de.rettichlp.pkutils.common.Storage;
 import de.rettichlp.pkutils.common.registry.PKUtilsBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsListener;
 import de.rettichlp.pkutils.common.services.HudService;
 import de.rettichlp.pkutils.listener.IHudRenderListener;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.text.MutableText;
 
+import java.awt.Color;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 import static de.rettichlp.pkutils.PKUtilsClient.hudService;
+import static de.rettichlp.pkutils.PKUtilsClient.storage;
+import static java.time.Duration.between;
+import static java.time.LocalDateTime.now;
 import static java.util.stream.Collectors.toMap;
+import static net.minecraft.text.Text.empty;
+import static net.minecraft.text.Text.of;
+import static net.minecraft.util.Formatting.DARK_GRAY;
+import static net.minecraft.util.Formatting.GRAY;
 
 @PKUtilsListener
 public class HudListener extends PKUtilsBase implements IHudRenderListener {
 
     @Override
     public void onHudRender(DrawContext drawContext, RenderTickCounter renderTickCounter) {
+        renderCountdowns(drawContext);
         renderNotifications(drawContext);
+    }
+
+    private void renderCountdowns(DrawContext drawContext) {
+        Map<Storage.Countdown, Long> activeCountdownRemainingMillis = storage.getCountdowns().entrySet().stream()
+                .filter(cooldownLocalDateTimeEntry -> {
+                    LocalDateTime cooldownStartTime = cooldownLocalDateTimeEntry.getValue();
+                    Storage.Countdown countdown = cooldownLocalDateTimeEntry.getKey();
+                    LocalDateTime countdownExpiredTime = cooldownStartTime.plus(countdown.getDuration());
+                    return now().isAfter(countdownExpiredTime); // the countdown is active
+                })
+                .collect(toMap(
+                        Map.Entry::getKey,
+                        entry -> between(now(), entry.getValue().plus(entry.getKey().getDuration())).toMillis()
+                ));
+
+        if (activeCountdownRemainingMillis.isEmpty()) {
+            return;
+        }
+
+        // build countdown strings
+        List<MutableText> countdownStrings = activeCountdownRemainingMillis.entrySet().stream()
+                .map(entry -> {
+                    String millisToFriendlyString = millisToFriendlyString(entry.getValue());
+                    return empty()
+                            .append(of(entry.getKey().getDisplayName()).copy().formatted(GRAY))
+                            .append(of(":").copy().formatted(DARK_GRAY)).append(" ")
+                            .append(of(millisToFriendlyString));
+                })
+                .toList();
+
+        // build text box
+        MutableText countdownText = empty();
+        countdownStrings.forEach(mutableText -> countdownText.append(mutableText).append(" "));
+
+        hudService.renderTextBox(
+                drawContext,
+                countdownText,
+                new Color(255, 255, 255, 255),
+                new Color(127, 127, 127, 100),
+                0);
     }
 
     private void renderNotifications(DrawContext drawContext) {
