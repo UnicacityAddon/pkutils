fd2fe13d398ee4ce1b70236a5b492aa3dc6a236b
RettichLP <rettichlp01@gmail.com>
2025-09-30 01:21:31 +0200

    refactor: clean up imports and add whitespace for readability

--- src/client/java/de/rettichlp/pkutils/common/models/ContractEntry.java
+++ src/client/java/de/rettichlp/pkutils/common/models/ContractEntry.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class ContractEntry {
+
     private String playerName;
     private int price;
 }
--- src/client/java/de/rettichlp/pkutils/listener/impl/faction/ContractListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/faction/ContractListener.java
@@ -6,7 +6,6 @@ import de.rettichlp.pkutils.common.registry.PKUtilsListener;
 import de.rettichlp.pkutils.listener.IMessageReceiveListener;
 import net.minecraft.text.Text;
 
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,25 +13,24 @@ import static de.rettichlp.pkutils.PKUtilsClient.storage;
 import static de.rettichlp.pkutils.PKUtilsClient.syncService;
 import static java.lang.Integer.parseInt;
 import static java.lang.System.currentTimeMillis;
-import static java.lang.System.getLogger;
 import static java.util.regex.Pattern.compile;
 
 @PKUtilsListener
 public class ContractListener extends PKUtilsBase implements IMessageReceiveListener {
+
     private static final Pattern CONTRACT_HEADER_PATTERN = compile("^\\[Contracts] Kopfgelder:"); //TODO
     private static final Pattern CONTRACT_ENTRY_PATTERN = compile("^(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) (?<price>.+) (?<afk> \\| (AFK)|)");//TODO
     private static final Pattern CONTRACT_ENTRY_ADD = compile("^\\[Contract] Es wurde ein Kopfgeld auf (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) (?<price>.[0-9]+) ausgesetzt.$");//TODO
     private static final Pattern CONTRACT_ENTRY_REMOVE = compile("^\\[Contract] (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) getötet.$");//TODO
+
     private long activeCheck = 0;
 
     @Override
     public boolean onMessageReceive(Text text, String message) {
-
         Matcher contractHeaderMatcher = CONTRACT_HEADER_PATTERN.matcher(message);
         if (contractHeaderMatcher.find()) {
             this.activeCheck = currentTimeMillis();
             storage.getContractEntries().clear();
-
             return !syncService.isGameSyncProcessActive();
         }
 
@@ -40,6 +38,7 @@ public class ContractListener extends PKUtilsBase implements IMessageReceiveList
         if (contractEntryMatcher.find() && (currentTimeMillis() - this.activeCheck < 100)) {
             String playerName = contractEntryMatcher.group("playerName");
             int price = parseInt(contractEntryMatcher.group("price"));
+
             ContractEntry contractEntry = new ContractEntry(playerName, price);
             storage.getContractEntries().add(contractEntry);
             return !syncService.isGameSyncProcessActive();
