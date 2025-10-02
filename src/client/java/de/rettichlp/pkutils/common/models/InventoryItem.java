0eaa888408a946145edb3602ef48faf6bfa920bf
RettichLP <rettichlp01@gmail.com>
2025-10-02 15:08:23 +0200

    refactor: remove SettingSection and item references from InventoryItem enum

--- src/client/java/de/rettichlp/pkutils/common/models/InventoryItem.java
+++ src/client/java/de/rettichlp/pkutils/common/models/InventoryItem.java
@@ -2,51 +2,34 @@ package de.rettichlp.pkutils.common.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
 import static java.util.Arrays.stream;
-import static net.minecraft.item.Items.FERN;
-import static net.minecraft.item.Items.GUNPOWDER;
-import static net.minecraft.item.Items.IRON_INGOT;
-import static net.minecraft.item.Items.LIME_DYE;
-import static net.minecraft.item.Items.PINK_DYE;
-import static net.minecraft.item.Items.QUARTZ;
-import static net.minecraft.item.Items.SUGAR;
-import static net.minecraft.item.Items.WITHER_SKELETON_SKULL;
 
 @Getter
 @AllArgsConstructor
 public enum InventoryItem {
 
     // drugs
-    POWDER("Pulver", true, SUGAR),
-    HERBS("Kräuter", true, FERN),
-    CRYSTALS("Kristalle", true, QUARTZ),
-    GRAB_BAG("Wundertüte", true, LIME_DYE),
+    POWDER("Pulver", true),
+    HERBS("Kräuter", true),
+    CRYSTALS("Kristalle", true),
+    GRAB_BAG("Wundertüte", true),
 
     // medical
-    COUGH_SYRUP("Hustensaft", false, PINK_DYE),
-    PAINKILLERS("Schmerzmittel", false, PINK_DYE),
-    ANTIBIOTICS("Antibiotika", false, PINK_DYE),
+    COUGH_SYRUP("Hustensaft", false),
+    PAINKILLERS("Schmerzmittel", false),
+    ANTIBIOTICS("Antibiotika", false),
 
     // other
-    MASK("Maske", false, WITHER_SKELETON_SKULL),
-    GUN_POWDER("Schwarzpulver", false, GUNPOWDER),
-    IRON("Eisen", false, IRON_INGOT);
+    MASK("Maske", false),
+    GUN_POWDER("Schwarzpulver", false),
+    IRON("Eisen", false);
 
     private final String displayName;
     private final boolean drugBankItem;
-    private final Item item;
-
-    @Contract(" -> new")
-    public @NotNull ItemStack getItemStack() {
-        return new ItemStack(this.item);
-    }
 
     public static @NotNull Optional<InventoryItem> fromDisplayName(String displayName) {
         return stream(values())
