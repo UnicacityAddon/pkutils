91694b7a9b4824de84028b1c342e7787079ecf95
RettichLP <rettichlp01@gmail.com>
2025-10-04 16:01:47 +0200

    fix: adjust regex pattern for optional AFK status in contract entries

--- src/client/java/de/rettichlp/pkutils/listener/impl/faction/ContractListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/faction/ContractListener.java
@@ -21,7 +21,7 @@ import static java.util.regex.Pattern.compile;
 public class ContractListener extends PKUtilsBase implements IMessageReceiveListener {
 
     private static final Pattern CONTRACT_HEADER_PATTERN = compile("^\\[Contracts] Kopfgelder:$");
-    private static final Pattern CONTRACT_ENTRY_PATTERN = compile("^(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) \\[(?<price>\\d+)\\$](?:\\(AFK\\))?$");
+    private static final Pattern CONTRACT_ENTRY_PATTERN = compile("^(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) \\[(?<price>\\d+)\\$](?: \\(AFK\\))?$");
     private static final Pattern CONTRACT_ADD_PATTERN = compile("^\\[Contract] Es wurde ein Kopfgeld auf (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) \\((?<price>\\d+)\\$\\) ausgesetzt\\.$");
     private static final Pattern CONTRACT_REMOVE_PATTERN = compile("^\\[Contract] Das Kopfgeld auf (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) wurde entfernt\\.$");
     private static final Pattern CONTRACT_KILL_PATTERN = compile("^\\[Contract] (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat (?:\\[PK])?(?<targetName>[a-zA-Z0-9_]+) getötet\\. \\((?<price>\\d+)\\$\\)$");
@@ -38,7 +38,7 @@ public class ContractListener extends PKUtilsBase implements IMessageReceiveList
         }
 
         Matcher contractEntryMatcher = CONTRACT_ENTRY_PATTERN.matcher(message);
-        if (contractEntryMatcher.find() && (currentTimeMillis() - this.activeCheck < 100)) {
+        if (contractEntryMatcher.find() && currentTimeMillis() - this.activeCheck < 100) {
             String playerName = contractEntryMatcher.group("playerName");
             int price = parseInt(contractEntryMatcher.group("price"));
 
