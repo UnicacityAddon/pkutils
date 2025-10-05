5b4a1b7c0ffa1ee0ac8bde723b491e64d806ff21
RettichLP <rettichlp01@gmail.com>
2025-10-05 22:34:23 +0200

    feat: add jobCooldownEndTimes map to MainConfig for tracking job cooldowns

--- src/client/java/de/rettichlp/pkutils/common/models/config/MainConfig.java
+++ src/client/java/de/rettichlp/pkutils/common/models/config/MainConfig.java
@@ -1,16 +1,21 @@
 package de.rettichlp.pkutils.common.models.config;
 
 import de.rettichlp.pkutils.common.models.Faction;
+import de.rettichlp.pkutils.common.models.Job;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static de.rettichlp.pkutils.common.models.Faction.NULL;
+import java.util.Map;
 
 @Data
 public class MainConfig {
 
+    private final Map<Job, LocalDateTime> jobCooldownEndTimes = new HashMap<>();
     private List<TodoEntry> todos = new ArrayList<>();
     private Faction allianceFaction = NULL;
     private Options options = new Options();
