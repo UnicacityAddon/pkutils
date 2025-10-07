62f5263ffd67291aa478d0030f5e88380d5cb386
Fauliikou <156687315+Fauliikou@users.noreply.github.com>
2025-10-07 12:08:40 +0200

    feat: enhance PersonalUseCommand with message handling and command execution

--- src/client/java/de/rettichlp/pkutils/command/faction/PersonalUseCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/faction/PersonalUseCommand.java
@@ -4,23 +4,35 @@ import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.rettichlp.pkutils.common.registry.CommandBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
+import de.rettichlp.pkutils.listener.IMessageReceiveListener;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
+import javax.management.timer.Timer;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 import static com.mojang.brigadier.arguments.StringArgumentType.word;
-import static de.rettichlp.pkutils.PKUtilsClient.configService;
-import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
+import static de.rettichlp.pkutils.PKUtilsClient.*;
 import static java.lang.String.valueOf;
+import static java.util.regex.Pattern.compile;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 import static net.minecraft.command.CommandSource.suggestMatching;
 
 @PKUtilsCommand(label = "eigenbedarf")
-public class PersonalUseCommand extends CommandBase {
+public class PersonalUseCommand extends CommandBase implements IMessageReceiveListener {
+
+    private static final Pattern DEAL_ACCEPTED = compile("^\\[Deal] (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat den Deal angenommen\\.$");
+    private static final Pattern DEAL_DECLINED = compile("^(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat das Angebot abgelehnt\\.$");
+
+    public static List<String> commands = new ArrayList<>();
 
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
@@ -36,7 +48,14 @@ public class PersonalUseCommand extends CommandBase {
                                 })
                                 .executes(context -> {
                                     String targetPlayer = getString(context, "player");
-                                    sendCommands(createCommands("selldrug " + targetPlayer + " %name% %amount% %purity% 0"));
+
+                                    commands = new ArrayList<>(createCommands("selldrug " + targetPlayer + " %name% %amount% %purity% 0"));
+
+                                    if (!commands.isEmpty()) {
+                                        String s = commands.removeFirst();
+                                        sendCommand(s);
+                                    }
+
                                     return 1;
                                 })))
                 .executes(context -> {
@@ -60,4 +79,19 @@ public class PersonalUseCommand extends CommandBase {
 
         return commandStrings;
     }
+
+    @Override
+    public boolean onMessageReceive(Text text, String message) {
+        if (DEAL_ACCEPTED.matcher(message).find() || DEAL_DECLINED.matcher(message).find()) {
+
+            if (!commands.isEmpty()) {
+                String s = commands.removeFirst();
+                sendCommand(s);
+            }
+
+            return true;
+        }
+        return false;
+    }
+
 }
