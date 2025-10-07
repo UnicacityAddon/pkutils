e9841d0b670175551f8dbe0b01b64dd617cb67e9
RettichLP <rettichlp01@gmail.com>
2025-10-07 20:56:56 +0200

    refactor: encapsulate command handling in PersonalUseCommand

--- src/client/java/de/rettichlp/pkutils/command/faction/PersonalUseCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/faction/PersonalUseCommand.java
@@ -6,15 +6,12 @@ import de.rettichlp.pkutils.common.registry.CommandBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
 import de.rettichlp.pkutils.listener.IMessageReceiveListener;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
-import javax.management.timer.Timer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
@@ -32,7 +29,7 @@ public class PersonalUseCommand extends CommandBase implements IMessageReceiveLi
     private static final Pattern DEAL_ACCEPTED = compile("^\\[Deal] (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat den Deal angenommen\\.$");
     private static final Pattern DEAL_DECLINED = compile("^(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat das Angebot abgelehnt\\.$");
 
-    public static List<String> commands = new ArrayList<>();
+    private List<String> commands = new ArrayList<>();
 
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
@@ -48,14 +45,8 @@ public class PersonalUseCommand extends CommandBase implements IMessageReceiveLi
                                 })
                                 .executes(context -> {
                                     String targetPlayer = getString(context, "player");
-
-                                    commands = new ArrayList<>(createCommands("selldrug " + targetPlayer + " %name% %amount% %purity% 0"));
-
-                                    if (!commands.isEmpty()) {
-                                        String s = commands.removeFirst();
-                                        sendCommand(s);
-                                    }
-
+                                    this.commands = new ArrayList<>(createCommands("selldrug " + targetPlayer + " %name% %amount% %purity% 0"));
+                                    removeAndExecuteFirst();
                                     return 1;
                                 })))
                 .executes(context -> {
@@ -83,15 +74,16 @@ public class PersonalUseCommand extends CommandBase implements IMessageReceiveLi
     @Override
     public boolean onMessageReceive(Text text, String message) {
         if (DEAL_ACCEPTED.matcher(message).find() || DEAL_DECLINED.matcher(message).find()) {
-
-            if (!commands.isEmpty()) {
-                String s = commands.removeFirst();
-                sendCommand(s);
-            }
-
-            return true;
+            removeAndExecuteFirst();
         }
-        return false;
+
+        return true;
     }
 
+    private void removeAndExecuteFirst() {
+        if (!this.commands.isEmpty()) {
+            String firstCommandString = this.commands.removeFirst();
+            sendCommand(firstCommandString);
+        }
+    }
 }
