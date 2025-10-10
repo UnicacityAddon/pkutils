e771e288ff6821337395d909f520aad0dc65e677
RettichLP <rettichlp01@gmail.com>
2025-10-10 16:54:06 +0200

    chore: rename files and update package imports for consistency

--- src/main/java/de/rettichlp/pkutils/PKUtils.java
+++ src/main/java/de/rettichlp/pkutils/PKUtils.java
@@ -1,10 +1,26 @@
 package de.rettichlp.pkutils;
 
-import net.fabricmc.api.ModInitializer;
+import de.rettichlp.pkutils.common.Storage;
+import de.rettichlp.pkutils.common.api.Api;
+import de.rettichlp.pkutils.common.models.config.Configuration;
+import de.rettichlp.pkutils.common.registry.Registry;
+import de.rettichlp.pkutils.common.services.FactionService;
+import de.rettichlp.pkutils.common.services.NotificationService;
+import de.rettichlp.pkutils.common.services.RenderService;
+import de.rettichlp.pkutils.common.services.SyncService;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PKUtils implements ModInitializer {
+import static java.util.Objects.isNull;
+
+public class PKUtils implements ClientModInitializer {
 
     public static final String MOD_ID = "pkutils";
 
@@ -13,12 +29,64 @@ public class PKUtils implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    @Override
-    public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
+    public static final Api api = new Api();
+    public static final Storage storage = new Storage();
+    public static final Configuration configuration = new Configuration().loadFromFile();
 
-        // LOGGER.info("Hello Fabric world!");
+    public static ClientPlayerEntity player;
+    public static ClientPlayNetworkHandler networkHandler;
+
+    public static FactionService factionService = new FactionService();
+    public static NotificationService notificationService = new NotificationService();
+    public static RenderService renderService = new RenderService();
+    public static SyncService syncService = new SyncService();
+
+    private final Registry registry = new Registry();
+
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+        this.registry.registerSounds();
+
+        // parse blacklist reasons from GitHub Gist
+        syncService.syncBlacklistReasonData();
+        // parse faction data
+        syncService.syncFactionData();
+        // login to PKUtils API
+        syncService.syncPKUtilsData();
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            player = client.player;
+            networkHandler = handler;
+
+            storage.setPunicaKitty(isPunicaKitty());
+            client.execute(this.registry::registerListeners);
+        });
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            this.registry.registerCommands(dispatcher);
+        });
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> configuration.saveToFile());
+    }
+
+    private boolean isPunicaKitty() {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+        if (isNull(networkHandler)) {
+            LOGGER.warn("Not connected to PunicaKitty: Network handler is null");
+            return false;
+        }
+
+        String addressString = networkHandler.getConnection().getAddress().toString(); // tcp.punicakitty.de./50.114.4.xxx:25565
+        // for LabyMod players, there is no dot at the end of the domain
+        if (!addressString.matches("tcp\\.punicakitty\\.de\\.?/50\\.114\\.4\\.\\d+:25565")) {
+            LOGGER.warn("Not connected to PunicaKitty: {}", addressString);
+            return false;
+        }
+
+        return true;
     }
 }
