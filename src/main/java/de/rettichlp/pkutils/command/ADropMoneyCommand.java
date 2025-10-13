c284dea25b2989c28cedb6a2b7a2ccae3104c964
Kyubiilikesweat <116468020+Kyubiilikesweat@users.noreply.github.com>
2025-10-13 21:27:04 +0200

    ADropMoneyCommand rework: added intelligent moneydrop

--- src/main/java/de/rettichlp/pkutils/command/ADropMoneyCommand.java
+++ src/main/java/de/rettichlp/pkutils/command/ADropMoneyCommand.java
@@ -4,9 +4,13 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.rettichlp.pkutils.common.registry.CommandBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+
+import net.minecraft.scoreboard.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+
+import static de.rettichlp.pkutils.PKUtils.*;
 
 @PKUtilsCommand(label = "adropmoney")
 public class ADropMoneyCommand extends CommandBase {
@@ -15,7 +19,24 @@ public class ADropMoneyCommand extends CommandBase {
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
         return node
                 .executes(context -> {
-                    sendCommands(List.of("bank abbuchen 16000", "dropmoney", "bank einzahlen 16000"));
+                    ScoreboardDisplaySlot slot = ScoreboardDisplaySlot.SIDEBAR;
+
+                    if (player.getScoreboard().getScore(ScoreHolder.fromName("§9Geld§8:"), player.getScoreboard().getObjectiveForSlot(slot)) == null) {
+                        sendModMessage("§cEs wurde kein Geldtransport-Job gefunden.", true);
+                        LOGGER.warn("ADropMoney: Scoreboard not found. ERROR: null");
+                        return 1;
+                    }
+
+                    int dropamount = player.getScoreboard().getScore(ScoreHolder.fromName("§9Geld§8:"),
+                            player.getScoreboard().getObjectiveForSlot(slot)).getScore();
+                    if (dropamount <= configuration.getMoneyBankAmount()){
+                        sendCommands(List.of("bank abbuchen " + dropamount, "dropmoney", "bank einzahlen " + dropamount));
+                        return 1;
+                    }
+                    if (dropamount > 0){
+                        int playermoney = configuration.getMoneyBankAmount();
+                        sendCommands(List.of("bank abbuchen " + playermoney, "dropmoney", "bank einzahlen " + playermoney, "adropmoney"));
+                    }
                     return 1;
                 });
     }
