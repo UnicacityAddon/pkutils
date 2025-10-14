adc44aa9f511b0773f5c224538fb4e67b2aca99e
RettichLP <rettichlp01@gmail.com>
2025-10-15 00:58:07 +0200

    feat: add bodyMapper method to IRequest and FactionPostRequest for JSON serialization

--- src/main/java/de/rettichlp/pkutils/common/api/IRequest.java
+++ src/main/java/de/rettichlp/pkutils/common/api/IRequest.java
@@ -1,8 +1,17 @@
 package de.rettichlp.pkutils.common.api;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.net.URI;
+
+import static de.rettichlp.pkutils.PKUtils.api;
 
 public interface IRequest {
 
     URI getUrl();
+
+    @Nullable
+    default String bodyMapper() {
+        return api.getGson().toJson(this);
+    }
 }
--- src/main/java/de/rettichlp/pkutils/common/api/request/FactionPostRequest.java
+++ src/main/java/de/rettichlp/pkutils/common/api/request/FactionPostRequest.java
@@ -4,6 +4,7 @@ import de.rettichlp.pkutils.common.api.IRequest;
 import de.rettichlp.pkutils.common.models.FactionEntry;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.net.URI;
 import java.util.Set;
@@ -17,5 +18,10 @@ public record FactionPostRequest(Set<FactionEntry> factionEntries) implements IR
     @Override
     public @NotNull URI getUrl() {
         return create(api.getBaseUrl() + "/factions");
+    }
+
+    @Override
+    public @Nullable String bodyMapper() {
+        return api.getGson().toJson(this.factionEntries);
     }
 }
