ac94622ad341910f1afec9a3e09b30afe8bc1566
RettichLP <rettichlp01@gmail.com>
2025-10-16 02:16:21 +0200

    feat: enhance service tracking by adding handling for no active services

--- src/main/java/de/rettichlp/pkutils/listener/impl/faction/EmergencyServiceListener.java
+++ src/main/java/de/rettichlp/pkutils/listener/impl/faction/EmergencyServiceListener.java
@@ -24,6 +24,7 @@ public class EmergencyServiceListener extends PKUtilsBase implements IMessageRec
     private static final Pattern SERVICE_ACCEPTED_PATTERN = compile("^(?:HQ: )?(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat den Notruf von (?:\\[PK])?(?<senderName>[a-zA-Z0-9_]+) angenommen\\.$");
     private static final Pattern SERVICE_REQUEUED_PATTERN = compile("^(?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) hat den Notruf von (?:\\[PK])?(?<senderName>[a-zA-Z0-9_]+) erneut geöffnet\\.$");
     private static final Pattern SERVICE_DONE_PATTERN = compile("^Du hast den Service von (?:\\[PK])?(?<playerName>[a-zA-Z0-9_]+) als 'Erledigt' markiert\\.$");
+    private static final Pattern SERVICE_NONE_PATTERN = compile("^Offene Notrufe \\(0\\)$|^Fehler: Es ist kein Service offen\\.$");
 
     private boolean activeService = false;
 
@@ -38,11 +39,11 @@ public class EmergencyServiceListener extends PKUtilsBase implements IMessageRec
 
         Matcher serviceAcceptedMatcher = SERVICE_ACCEPTED_PATTERN.matcher(message);
         if (serviceAcceptedMatcher.find()) {
-            String playerName = serviceAcceptedMatcher.group("playerName");
+            storage.setActiveServices(max(0, storage.getActiveServices() - 1));
 
+            String playerName = serviceAcceptedMatcher.group("playerName");
             if (playerName.equals(player.getGameProfile().getName())) {
                 this.activeService = true;
-                storage.setActiveServices(max(0, storage.getActiveServices() - 1));
             }
 
             return true;
@@ -50,11 +51,11 @@ public class EmergencyServiceListener extends PKUtilsBase implements IMessageRec
 
         Matcher serviceRequeuedMatcher = SERVICE_REQUEUED_PATTERN.matcher(message);
         if (serviceRequeuedMatcher.find()) {
-            String playerName = serviceRequeuedMatcher.group("playerName");
+            storage.setActiveServices(storage.getActiveServices() + 1);
 
+            String playerName = serviceRequeuedMatcher.group("playerName");
             if (playerName.equals(player.getGameProfile().getName())) {
                 this.activeService = false;
-                storage.setActiveServices(storage.getActiveServices() + 1);
             }
 
             return true;
@@ -64,6 +65,12 @@ public class EmergencyServiceListener extends PKUtilsBase implements IMessageRec
         if (serviceDoneMatcher.find()) {
             api.postActivityAdd(EMERGENCY_SERVICE);
             this.activeService = false;
+            return true;
+        }
+
+        Matcher serviceNoneMatcher = SERVICE_NONE_PATTERN.matcher(message);
+        if (serviceNoneMatcher.find()) {
+            storage.setActiveServices(0);
             return true;
         }
 
