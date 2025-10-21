aef83de72991178fb9e12e8589c551ede96a04f4
RettichLP <rettichlp01@gmail.com>
2025-10-21 16:58:34 +0200

    feat: rename InventoryScreenMixin to ScreenHandlerMixin and add GenericContainerScreenMixin for ABuy gui functionality

---
+++ src/main/java/de/rettichlp/pkutils/mixin/GenericContainerScreenMixin.java
@@ -0,0 +1,77 @@
+package de.rettichlp.pkutils.mixin;
+
+import de.rettichlp.pkutils.common.gui.options.components.ToggleButtonWidget;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.TextWidget;
+import net.minecraft.client.util.Window;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static de.rettichlp.pkutils.PKUtils.storage;
+import static java.lang.Integer.max;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+import static net.minecraft.text.Text.empty;
+import static net.minecraft.text.Text.of;
+
+@Mixin(GenericContainerScreen.class)
+public abstract class GenericContainerScreenMixin extends HandledScreen<GenericContainerScreenHandler> {
+
+    public GenericContainerScreenMixin(GenericContainerScreenHandler handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onContainerInit(CallbackInfo ci) {
+        TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
+        Window window = MinecraftClient.getInstance().getWindow();
+
+        int scaledWidth = window.getScaledWidth();
+        int scaledHeight = window.getScaledHeight();
+
+        int widgetHeight = 20;
+
+        int y = (scaledHeight - this.backgroundHeight) / 2 - widgetHeight - 4;
+        int buttonX = (scaledWidth - this.backgroundWidth) / 2;
+        int buttonWidth = this.backgroundWidth / 2; // 3/6
+        int textX = buttonX + buttonWidth;
+        int textWidth = this.backgroundWidth / 3; // 2/6
+        int textFieldX = textX + textWidth;
+        int textFieldWith = this.backgroundWidth / 6; // 1/6
+
+        ToggleButtonWidget toggleButton = new ToggleButtonWidget(of("ABuy"), storage::setABuyEnabled, storage.isABuyEnabled());
+        toggleButton.setX(buttonX);
+        toggleButton.setY(y);
+        toggleButton.setWidth(buttonWidth);
+        addDrawableChild(toggleButton);
+
+        TextWidget textWidget = new TextWidget(textX, y, textWidth, 20, of("Anzahl:"), textRenderer);
+        addDrawableChild(textWidget);
+
+        TextFieldWidget textField = new TextFieldWidget(textRenderer, textFieldX, y, textFieldWith, widgetHeight, empty());
+        textField.setMaxLength(2);
+        textField.setPlaceholder(of(valueOf(storage.getABuyAmount())));
+        textField.setChangedListener(this::onTextFieldChange);
+        addDrawableChild(textField);
+    }
+
+    @Unique
+    private void onTextFieldChange(String input) {
+        try {
+            int parsedInt = parseInt(input);
+            storage.setABuyAmount(max(1, parsedInt));
+        } catch (NumberFormatException e) {
+            storage.setABuyAmount(10);
+        }
+    }
+}
---
+++ src/main/java/de/rettichlp/pkutils/mixin/ScreenHandlerMixin.java
@@ -0,0 +1,80 @@
+package de.rettichlp.pkutils.mixin;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.listener.ServerPlayPacketListener;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.screen.slot.SlotActionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static de.rettichlp.pkutils.PKUtils.networkHandler;
+import static de.rettichlp.pkutils.PKUtils.storage;
+import static de.rettichlp.pkutils.PKUtils.utilsService;
+import static java.util.Objects.isNull;
+import static net.minecraft.screen.slot.SlotActionType.PICKUP;
+
+@Mixin(ScreenHandler.class)
+public class ScreenHandlerMixin {
+
+    @Unique
+    private static final int A_BUY_DELAY = 150;
+
+    @Inject(
+            method = "onSlotClick",
+            at = @At("HEAD")
+    )
+    private void onSlotClickMixin(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+        ScreenHandler handler = (ScreenHandler) (Object) this;
+
+        if (!storage.isABuyEnabled() || slotIndex < 0 || slotIndex >= handler.slots.size() || actionType != PICKUP) {
+            return;
+        }
+
+        int aBuyAmount = storage.getABuyAmount();
+        ClientPlayerInteractionManager interactionManager = MinecraftClient.getInstance().interactionManager;
+        Slot clickedSlot = handler.slots.get(slotIndex);
+        ItemStack itemStack = clickedSlot.getStack();
+
+        if (aBuyAmount <= 1 || isNull(interactionManager) || itemStack.isEmpty()) {
+            return;
+        }
+
+        Int2ObjectMap<ItemStack> stackMap = new Int2ObjectOpenHashMap<>();
+        for (int i = 0; i < handler.slots.size(); i++) {
+            stackMap.put(i, handler.getSlot(i).getStack().copy());
+        }
+
+        Packet<ServerPlayPacketListener> packet = new ClickSlotC2SPacket(
+                handler.syncId,
+                handler.getRevision(),
+                clickedSlot.id,
+                0,
+                PICKUP,
+                itemStack,
+                stackMap);
+
+        for (int i = 1; i < aBuyAmount; i++) {
+            utilsService.delayedAction(() -> {
+                // check if the same inventory is still open
+                Screen currentScreen = MinecraftClient.getInstance().currentScreen;
+                if (isNull(currentScreen)) {
+                    return;
+                }
+
+                networkHandler.sendPacket(packet);
+            }, 150L * i);
+        }
+    }
+}
