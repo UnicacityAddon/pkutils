3e303dcf12e97946212fe0955e67aed204874138
RettichLP <rettichlp01@gmail.com>
2026-09-06 21:46:58 +0200

    Add support for tracking blessings with countdown timer

--- src/main/java/de/rettichlp/ucutils/listener/impl/ChurchListener.java
+++ src/main/java/de/rettichlp/ucutils/listener/impl/ChurchListener.java
@@ -1,20 +1,26 @@
 package de.rettichlp.ucutils.listener.impl;
 
+import de.rettichlp.ucutils.common.models.Countdown;
 import de.rettichlp.ucutils.common.registry.UCUtilsListener;
 import de.rettichlp.ucutils.listener.IMessageReceiveListener;
 import net.minecraft.network.chat.Component;
 
+import java.time.Duration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static de.rettichlp.ucutils.UCUtils.commandService;
 import static de.rettichlp.ucutils.UCUtils.utilService;
+import static java.time.Duration.ofMinutes;
 import static java.util.regex.Pattern.compile;
 
 @UCUtilsListener
 public class PrayListener implements IMessageReceiveListener {
 
     private static final Pattern PRAYING_START_PATTERN = compile("^\\[Kirche] Du hast begonnen für (?:\\[UC])?(?<playerName>[a-zA-Z0-9_]+) zu beten\\.$");
+    private static final Pattern BLESSING_GIVEN_PATTERN = compile("^\\[Segen] Du hast (?:\\[UC])?(?<playerName>[a-zA-Z0-9_]+) gesegnet\\.$");
+
+    private static final Duration BLESSING_DURATION = ofMinutes(2);
 
     @Override
     public boolean onMessageReceive(Component text, String message) {
@@ -22,6 +28,12 @@ public class PrayListener implements IMessageReceiveListener {
         if (prayingStartMatcher.find()) {
             String playerName = prayingStartMatcher.group("playerName");
             utilService.delayedAction(() -> commandService.sendCommandWithAfkCheck("beten " + playerName), 15000);
+            return true;
+        }
+
+        Matcher blessingGivenMatcher = BLESSING_GIVEN_PATTERN.matcher(message);
+        if (blessingGivenMatcher.find()) {
+            new Countdown("Segen", BLESSING_DURATION);
             return true;
         }
 
